fdb37415bd04b5e4042bd74391d33fe63e9bb813
keith_irby <keithirby00@gmail.com>
2023-10-23 12:45:45 -0700

    Updating i2c test with proper comments, writting too much matlab code recently

--- lab_2/i2c_test.py
+++ lab_2/i2c_test.py
@@ -2,37 +2,37 @@ import board
 import digitalio
 import busio
 
-%Function that checks if our input and output is working
+#Function that checks if our input and output is working
 def check_io(): 
     pin = digitalio.DigitalInOut(board.D4)
     
-    %If the io object was created and didnt return none, it worked
+    #If the io object was created and didnt return none, it worked
     if pin: 
         print("IO is good!", pin)
     else: 
-    %Otherwise a io object was not created and io is not working
+    #Otherwise a io object was not created and io is not working
         print("IO is not working...")
 
 
-%Function that checks if our i2c is working
+#Function that checks if our i2c is working
 def check_i2c(): 
-    %Create a i2c object
+    #Create a i2c object
     i2c = busio.I2C(board.SCL, board.SDA)
     
-    %If the i2c object was created and didnt return none, it worked
+    #If the i2c object was created and didnt return none, it worked
     if i2c: 
         print("i2c is good!", i2c)
     else:
-    %Otherwise a i2c object was not created and i2c isnt working
+    #Otherwise a i2c object was not created and i2c isnt working
         print("i2c is not working...")
 
 def main(): 
-    %Initial display to the user if the checks crash
-    print("Hello! Checking board connections  connection...")
-    %Call io check
+    #Initial display to the user if the checks crash
+    print("Hello! Checking board connections connection...")
+    #Call io check
     check_io()
-    %Call i2c check
+    #Call i2c check
     check_i2c()
 
-%Call main function
+#Call main function
 main()

08a40531f1a9a65f5486322743211f042945a7e5
Keith Irby <keithirby00@gmail.com>
2023-10-23 18:17:53 -0700

    Adding file writing abilities and forever loop for writing

--- lab_2/sensor-polling.py
+++ lab_2/sensor-polling.py
@@ -1,6 +1,7 @@
 from datetime import datetime, date #For getting current date and time
 import os #For getting actual path when messing with files
 from i2c_class import i2c_controller
+import time
 
 
 def grab_name() -> str: 
@@ -30,33 +31,63 @@ def create_I2C_controller():
 	return controller 
 
 def poll_sensors(i2c_cont)->[]:
-    sensor_str_1 = "Temperature: " + i2c_cont.getTemp() + "C"
-    sensor_str_2 = "Humidity: "+ i2c_cont.getHumd() + "%"
-    sensor_str_3 = "Soil Moisture: " + i2c_cont.getSoilTemp() + "C"
-    sensor_str_4 = "Soil Temperature: " + i2c_cont.getSoilMoist()
-    sensor_str_5 = "Wind Speed: "+ i2c_cont.map_volt_value(i2c_cont.getADCVoltage()) + "m/s"
+    """
+    Polls all the important sensors reading together and combines and returns a list of them
+    """
+    #Make a empty list of size 5 for 5 sensor polls
+    poll_strs = [None] * 5
+    #Fill in the polls list
+    poll_strs[0] = "Temperature: " + i2c_cont.getTemp() + "C" + "\n"
+    poll_strs[1] = "Humidity: "+ i2c_cont.getHumd() + "%" + "\n"
+    poll_strs[2] = "Soil Moisture: " + i2c_cont.getSoilTemp() + "C" + "\n"
+    poll_strs[3] = "Soil Temperature: " + i2c_cont.getSoilMoist() + "\n"
+    poll_strs[4] = "Wind Speed: "+ i2c_cont.map_volt_value(i2c_cont.getADCVoltage()) + "m/s" + "\n"
+    #Return the polls list
+    return poll_strs
       
-def write_file(): 
+def write_item(file_path, write_item): 
     """
-    This function is dedicated to writing to a file
+    This function is dedicated to writing one item to a file
     """
-    #Setting the path to save the file to
-    save_path = os.path.expanduser("~/Desktop/")
-    file_name = "report.txt"
-    complete_path = os.path.join(save_path, file_name)
     #Write to a file
-    with open(complete_path, "w") as file: 
-         file.writelines
+    with open(file_path, "w") as file: 
+         file.writelines(write_item)
 
+def write_items(file_path, write_items): 
+    """
+    This function is dedicated to writing multiple items to a file
+    """
+    #Write to a file
+    with open(file_path, "w") as file: 
+         for i in write_items:
+            file.writelines(i)
 
 #Main function that calls all other functions 
 def main():
 	#Get the I2C device controller
     i2c_cont = create_I2C_controller()
+
+    #Setting the name of the file
+    file_name = "polling-log.txt"
+    #Setting the path to save the file to
+    save_path = os.path.expanduser("~/Desktop/")
+    complete_path = os.path.join(save_path, file_name)
+    #Quickly write name to file once
+    write_item(complete_path, grab_name())
+
+
     
     #Start the sensor polling
     print("Hello there! commencing sensor polling...")
-    poll_sensors(i2c_cont)
+    
+    num_polls = 6 # 1 date and time poll + 5 sensor polls = 6 polls 
+
+    #While loop that continues until the program is cancelled to write to a file every 5 seconds
+    while(1):
+        write_item(complete_path, grab_date_time())
+        write_items(complete_path, poll_sensors(i2c_cont))
+        time.sleep(5)
+
 
 	
     
